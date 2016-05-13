e6be502110a3688ab27d073672426073f61b587d
tuvior <tobiasuzt@gmail.com>
2016-05-13 12:43:07 +0200

    jambon

--- reciever.m
+++ reciever.m
@@ -1,4 +1,4 @@
-% sample frequency
+% sample frequencty
 Fs = 44100;
 
 nBits = 8;
@@ -12,8 +12,26 @@ nChannels = 1;
 %rec_data = reshape(rec_data, 9736);
 
 temp = reshape(sound_data, length(t), []);
-
 [maxValue, indexMax] = max(abs(fft(temp - mean(temp(:)))));
 frequency = (indexMax-1) * Fs / length(t);
+
+b_string = [];
+
+for i=frequency
+    delta = i - b_f;
+    
+    if delta > 0
+        b_string = [b_string, '0'];
+    else 
+        b_string = [b_string, '1'];
+    end
+end
+
+S = transpose(reshape(b_string, 8, []));
+decimalValues = bin2dec(S);
+
+out = char(decimalValues);
+    
+
 %plot(rec_data);
 %axis([0 200000 -1 1]);
