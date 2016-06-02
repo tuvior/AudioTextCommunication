943e4874e651c7178bb9ddbc0707e2d8fc98b1da
tuvior <tobiasuzt@gmail.com>
2016-06-02 14:37:18 +0200

    removed plot

--- analyse_noise.m
+++ analyse_noise.m
@@ -7,7 +7,6 @@ recordblocking(rec, 5);
 noise = getaudiodata(rec, 'double');
 
 [ft, f] = periodogram(noise, [], [], Fs);
-plot(ft);
 inx = floor(200 * length(ft) / Fs);
 [~, in] = findpeaks(ft(inx:end), 'SortStr', 'descend', 'NPeaks', 2);
 freq = sort(f(in + inx));
